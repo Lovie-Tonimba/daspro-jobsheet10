9a174a7575d434f2381e35b57a6557dba1709bb5
Lovie-Tonimba <jechonialovie@gmail.com>
2024-11-22 13:05:11 +0700

    modify no.12

--- Cinema244107060101.java
+++ Cinema244107060101.java
@@ -17,9 +17,8 @@ public class Cinema244107060101 {
         for (String[] rowAudience : audience) {
             System.out.println("Length of row: " + rowAudience.length);
         }
-        System.out.println("Audience in the row 3: ");
-        for (String i : audience[2]) {
-            System.out.println((i));
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the tow: " + (i + 1) + String.join(  ", ", audience[i]));
         }
     }
 }
